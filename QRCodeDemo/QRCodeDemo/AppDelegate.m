198d49eb053b1b30a775e18fa75e56660a0cb53d
徐亚辉 <>
2018-05-31 14:41:03 +0800

    test001

--- QRCodeDemo/QRCodeDemo/AppDelegate.m
+++ QRCodeDemo/QRCodeDemo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"test001");
+    NSLog(@"test001");
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     
