440abb1210b3d5d0b0393bbbbb362b4201db2777
徐亚辉 <1181478599@qq.com>
2018-06-02 17:22:47 +0800

    nslog

--- QRCodeDemo/QRCodeDemo/AppDelegate.m
+++ QRCodeDemo/QRCodeDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"master");
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     
